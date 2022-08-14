69ef4d45ee901eb02944b4a7cdb7266a2acd411a
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 17:45:26 +0100

    ft_memset.c

--- lvl_0_libft/ft_memset.c
+++ lvl_0_libft/ft_memset.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 17:31:46 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/14 16:36:48 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/14 17:43:33 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#include "libft.h"
 
-#include <unistd.h>
+void	*ft_memset(void *ptr, int x, size_t n)
+{
+	char	*conv_ptr;
+	size_t	i;
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *ptr, int x, size_t n);
-
-void	ft_putchar_fd(char c, int fd);
-
-#endif
+	conv_ptr = (char *)ptr;
+	i = 0;
+	while (i < n)
+	{
+		*(conv_ptr + i) = x;
+		i++;
+	}
+	return (ptr);
+}
