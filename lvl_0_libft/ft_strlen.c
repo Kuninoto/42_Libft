0d1b3d115e750f6cb5cc4da643a4f33b0cd3395b
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 03:52:46 +0100

    ft_strlen.c

--- lvl_0_libft/ft_strlen.c
+++ lvl_0_libft/ft_strlen.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 03:51:59 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/14 03:38:43 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/14 03:52:02 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <unistd.h>
+size_t  ft_strlen(const char *str)
+{
+    size_t  i;
 
-int     ft_isalpha(int c);
-int     ft_isdigit(int c);
-int     ft_isalnum(int c);
-int     ft_isascii(int c);
-int     ft_isprint(int c);
-size_t  ft_strlen(const char *str);
-
-void    ft_putchar_fd(char c, int fd);
-
-#endif
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
