5fe23659048c7284846a6f1c1bd08c769e9b3f93
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 23:32:07 +0100

    Norm_corrections

--- lvl_0_libft/ft_strncmp.c
+++ lvl_0_libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 23:11:48 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 23:29:34 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/14 23:31:49 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while ((str1[i] == str2[i]) && (str1[i] != '\0' && str2[i] != '\0') && i <= (n - 1))
+	while ((str1[i] == str2[i]) && (str1[i] != '\0' && str2[i] != '\0')
+		&& i <= (n - 1))
 		i++;
 	return ((unsigned char )str1[i] - (unsigned char)str2[i]);
 }
