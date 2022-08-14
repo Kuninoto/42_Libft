54ad4c41b07e3e401ddb82fcc9a5fef3391c6ba1
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 23:08:12 +0100

    Some_corrections_and_ft_strrchr.c

--- lvl_0_libft/ft_strrchr.c
+++ lvl_0_libft/ft_strrchr.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/14 21:54:29 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 23:06:59 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/14 22:08:35 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/14 23:06:53 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strrchr(const char *str, int c)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
+	int	j;
+	
+	j = ft_strlen (str);
+	if (c == '\0')
+		return ((char *)str + j);
+	while (j >= 0)
 	{
-		if (str[i] == c)
-			return ((char *)str + i);
-		i++;
+		if (str[j] == c)
+		{
+			return ((char*)str + j);
+		}
+		j--;
 	}
 	return (NULL);
 }
--- lvl_0_libft/libft.h
+++ lvl_0_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 21:54:47 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/14 22:42:46 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 
 void	ft_putchar_fd(char c, int fd);
 
