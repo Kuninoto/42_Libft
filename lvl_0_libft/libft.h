2a6e185029c84030bee4f429e13e159e56d36aef
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 21:10:37 +0100

    ft_strlcat

--- lvl_0_libft/libft.h
+++ lvl_0_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 18:56:05 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/14 20:40:13 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	ft_bzero(void *ptr, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, char *src, size_t size);
 
 void	ft_putchar_fd(char c, int fd);
 
