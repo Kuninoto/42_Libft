c8d39617376cfe1112c8327d994da248fd9cd815
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-14 18:28:24 +0100

    ft_memcpy.c

--- lvl_0_libft/libft.h
+++ lvl_0_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 17:56:40 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/14 18:12:06 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *ptr, int x, size_t n);
 void	ft_bzero(void *ptr, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	ft_putchar_fd(char c, int fd);
 
