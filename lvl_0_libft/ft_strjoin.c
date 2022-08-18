f4c369369b4364b7411659700c0e3674f098c0bd
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-18 23:49:36 +0100

    ft_strjoin.c_NORMINETTE_FAILED

---
+++ lvl_0_libft/ft_strjoin.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 23:09:37 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/18 23:42:03 by nnuno-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+char	*strjoin_s1(char const *s1)
+{
+	size_t	len_s1;
+	len_s1 = ft_strlen(s1);
+}
+
+char	*strjoin_s2(char const *s2)
+{
+
+}
+*/
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*final_str;
+	size_t	i;
+	size_t	j;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	final_str = malloc((len_s1 + len_s2) * sizeof(char));
+	i = 0;
+	j = 0;
+	if (final_str == NULL)
+		return (NULL);
+	while (j < len_s1)
+	{
+		final_str[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (j < len_s2)
+	{
+		final_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	final_str[i] = '\0';
+	return (final_str);
+}
--- lvl_0_libft/libft.h
+++ lvl_0_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/18 01:50:40 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/18 23:16:57 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ int	ft_atoi(const char *str);
 void	*ft_calloc(size_t nitems, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 void	ft_putchar_fd(char c, int fd);
 
