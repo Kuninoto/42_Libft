7b822689982adf43e695cbe9f72f1d4d0de2701e
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-19 02:56:20 +0100

    Updates; ft_strjoin NORMINETTE FAILED; ft_strtrim with a comment to the solution thinking

---
+++ lvl_0_libft/ft_strtrim.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 23:51:37 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/19 02:54:39 by nnuno-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	// USAR FT_STRCHR e subdividir a string em beg e end, sendo a trimmed string o intervalo beg -> end
+}
