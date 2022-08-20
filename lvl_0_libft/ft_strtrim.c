8d0851ebf13f2a025514bd12c70b8d43354bf5a8
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-20 01:09:15 +0100

    ft_strtrim.c

--- lvl_0_libft/ft_strtrim.c
+++ lvl_0_libft/ft_strtrim.c
@@ -6,13 +6,54 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 23:51:37 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/19 02:54:39 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/20 01:08:12 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
+	return (dest);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	// USAR FT_STRCHR e subdividir a string em beg e end, sendo a trimmed string o intervalo beg -> end
+	size_t	beg;
+	size_t	end;
+	char	*trimmed_str;
+
+	if (*s1 == '\0' || *set == '\0')
+		return (NULL);
+	beg = 0;
+	while (s1[beg] != '\0' && ft_strchr(set, s1[beg]) != NULL)
+		beg++;
+	end = ft_strlen (s1 + beg);
+	if (end != '\0')
+	{
+		while (s1[beg + end - 1] != '\0' && ft_strchr(set, s1[beg + end - 1])
+			!= NULL)
+			end--;
+	}
+	trimmed_str = malloc(end + 1 * sizeof(char));
+	if (trimmed_str == NULL)
+		return (NULL);
+	ft_strncpy(trimmed_str, (s1 + beg), end);
+	return (trimmed_str);
 }
