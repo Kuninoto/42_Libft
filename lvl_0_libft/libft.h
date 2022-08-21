9d2870245a4fb8fc3e6853b9186272267bc8b771
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-21 18:21:42 +0100

    ft_strmapi.c

--- lvl_0_libft/libft.h
+++ lvl_0_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 01:35:12 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/21 02:54:04 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/21 18:21:13 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set); // uses ft_strncpy extra
 char	**ft_split(char const *s, char c); // uses ft_strncpy extra
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 void	ft_putchar_fd(char c, int fd);
 
