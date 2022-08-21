1334d8423bd10479fe3498a4f12a1f8f9a4212c6
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-21 17:33:28 +0100

    ft_itoa.c

--- lvl_0_libft/ft_split.c
+++ lvl_0_libft/ft_split.c
@@ -6,12 +6,11 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 01:10:01 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/21 14:32:21 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/21 16:04:38 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static unsigned int	ft_nr_strs(char const *s, char c)
 {
