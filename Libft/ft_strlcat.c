3f9819e9ad24dc95b2c0c9f763e6edce628b4f89
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-22 02:19:07 +0100

    Updates

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/14 19:07:10 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/14 21:09:37 by nnuno-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	len_dest;
+	size_t	len_src;
+	size_t	j_dest;
+	size_t	i;
+
+	len_dest = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	j_dest = len_dest;
+	i = 0;
+	if (size == 0)
+		return (len_src);
+	if (size < len_dest)
+		return (len_src + size);
+	while (src[i] != '\0' && j_dest < (size - 1))
+	{
+		dest[j_dest] = src[i];
+		j_dest++;
+		i++;
+	}
+	dest[j_dest] = '\0';
+	return (len_dest + len_src);
+}
