1a9671cc4a23768fe40023bbe3ddcc1a4da0e52f
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-22 02:19:07 +0100

    Updates

---
+++ Libft/Main_Tests.c
@@ -0,0 +1,77 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+//	char	str[]="O Nuno tem grande piroca";
+//	char	s1[]="Olá eu ";
+//	char	s2[]="sou o Nuno";
+//	char	s3[]="   !! !  !! O teu pai tem bigode !!! ";
+//	char	set[]=" !";
+//	char	split[]="A tua mãe.O teu pai.O Cavalo do teu tio.A Égua da tua tia.";
+
+//	printf("FT_STRLEN OF str[] is: %ld\n", ft_strlen(str));
+//	printf("FT_SUBSTR should be: tem grande piroc ->\t%s\n", ft_substr(str, 7, 16));
+//	printf("FT_STRJOIN should be: Olá eu sou o Nuno ->\t%s\n", ft_strjoin(s1, s2));
+//	printf("FT_STRTRIM should be: O teu pai tem bigode ->\t\"%s\"\n", ft_strtrim(s3, set));
+//	printf("FT_SPLIT should be:\n\tmatrix[0] = \"A tua mãe.\"\n\tmatrix[1] = \"O teu pai.\"\n\tmatrix[2] = \"O Cavalo do teu tio.\"\n\tmatrix[3] = \"A Égua da tua tia.\"\n\n");
+//	ft_split(split, '.');
+
+/****************************************************************FT_ITOA********************************************************************
+// MAX_INT_POSITIVE = 2147483647;
+		printf("MAX_INT_POSITIVE = %s\n", ft_itoa(2147483647));
+
+// MAX_INT_NEGATIVE = -2147483647 - 1;
+		printf("MAX_INT_NEGATIVE = %s\n", ft_itoa(-2147483648));
+
+
+	int	n;
+
+	n = 2147483647;
+	while (n != 0)
+	{
+		printf("%ld\n", n % 10);
+		n = n / 10;
+	}
+
+	int	n;
+
+	n = -2147483647;
+	while (n != 0)
+	{
+		printf("%d\n", n % 10);
+		n = n / 10;
+	}
+}
+*********************************************************************************************************************************************/
+/****************************************************************FT_PUTNBR_FD********************************************************************
+// MAX_INT_POSITIVE = 2147483647;
+		printf("MAX_INT_POSITIVE = %s\n", ft_putnbr_fd(2147483647));
+
+// MAX_INT_NEGATIVE = -2147483647 - 1;
+		printf("MAX_INT_NEGATIVE = %s\n", ft_putnbr_fd(-2147483648));
+
+
+	int	n;
+
+	n = 2147483647;
+	while (n != 0)
+	{
+		printf("%d\n", n % 10);
+		n = n / 10;
+	}
+
+	int	n;
+
+	n = -2147483647;
+	while (n != 0)
+	{
+		printf("%d\n", n % 10);
+		n = n / 10;
+	}
+}
+*********************************************************************************************************************************************/
+// MAX_INT_POSITIVE = 2147483647;
+// MAX_INT_NEGATIVE = -2147483647 - 1;
+}
---
+++ Libft/ft_split.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/20 01:10:01 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/21 16:04:38 by nnuno-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	ft_nr_strs(char const *s, char c)
+{
+	size_t	i;
+	size_t	nr_strs;
+
+	i = 0;
+	nr_strs = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			nr_strs++;
+		i++;
+	}
+	return (nr_strs);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t			i;
+	size_t			j;
+	size_t			lines;
+	unsigned int	nr_strs;
+	char			**matrix;
+
+	i = 0;
+	j = 0;
+	lines = 0;
+	nr_strs = ft_nr_strs(s, c);
+	matrix = malloc(nr_strs * sizeof(char *) + sizeof(NULL));
+	if (!matrix)
+		return (NULL);
+	while (lines < nr_strs)
+	{
+		while (s[j] != c)
+			j++;
+		matrix[lines] = ft_substr(s, i, (j - i) + 1);
+		j++;
+		i = j;
+		lines++;
+	}
+	matrix[lines] = NULL;
+	return (matrix);
+}
+
+/* j - i = s_len; s_len = tamanho da string que se pretende criar atualmente */
