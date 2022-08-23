92bcef90f5a8a582c70faa27670772d22577f956
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-23 01:27:15 +0100

    updates

--- lvl_0_libft/ft_memmove.c
+++ lvl_0_libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 18:30:57 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/14 18:48:06 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/23 01:06:28 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*conv_dest;
-	const char	*conv_src;
-	size_t		i;
+	char			*conv_dest;
+	const char		*conv_src;
+	size_t			i;
 
 	conv_dest = (char *)dest;
-	conv_src = (const char *)src;
+	conv_src = (char *)src;
 	i = 0;
 	if (!dest && !src)
 		return (NULL);
