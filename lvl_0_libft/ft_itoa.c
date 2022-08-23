81f44d201f7e7f36e4a6dc87f159e4a6d17e0522
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-23 19:55:51 +0100

    CORRECT-ft_itoa.c

--- lvl_0_libft/ft_itoa.c
+++ lvl_0_libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 02:47:05 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/21 17:33:16 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/23 19:55:09 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	ft_digits(int n)
 	int	digits;
 
 	digits = 0;
-	if (n < 0)
+	if (n <= 0)
 		digits++;
 	while (n != 0)
 	{
@@ -30,26 +30,27 @@ static int	ft_digits(int n)
 char	*ft_itoa(int n)
 {
 	size_t	digits;
-	size_t	digits_index;
 	int		signal;
 	char	*result;
 
 	digits = ft_digits(n);
-	digits_index = digits - 1;
 	signal = 1;
 	result = malloc((digits + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
 	result[digits] = '\0';
 	if (n < 0)
+	{
 		signal = -1;
+		result[0] = '-';
+	}
+	else if (n == 0)
+		result[0] = '0';
 	while (n != 0)
 	{
-		result[digits_index] = (n % 10 * signal) + '0';
+		digits--;
+		result[digits] = (n % 10 * signal) + '0';
 		n = n / 10;
-		digits_index--;
 	}
-	if (signal == -1)
-		result[digits_index] = '-';
 	return (result);
 }
