65211ecc8a81d56cb84792c399d854779b4a6a7c
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-23 23:42:07 +0100

    Updates

--- lvl_0_libft/ft_lstnew.c
+++ lvl_0_libft/ft_lstnew.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/14 17:58:28 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/23 23:41:40 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/22 20:22:43 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/22 21:57:13 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+t_list	*ft_lstnew(void *content)
 {
-	char		*conv_dest;
-	const char	*conv_src;
-	size_t		i;
-
-	if (!src && !dest)
-		return (NULL);
-	conv_dest = (char *)dest;
-	conv_src = (char *)src;
-	i = 0;
-	while (i < n)
-	{
-		*(conv_dest + i) = *(conv_src + i);
-		i++;
-	}
-	return (dest);
+	
 }
