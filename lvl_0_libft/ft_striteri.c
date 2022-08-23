b7e240c04d919ed813a2ef9f313db6faebde2c81
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-23 15:30:24 +0100

    Correction-ft_striteri.c

--- lvl_0_libft/ft_striteri.c
+++ lvl_0_libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 18:22:08 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/23 15:08:34 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/23 15:29:40 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,16 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	i = 0;
 	if (!s || !f)
 		return ;
-	while (s[i])
+	while (*s)
 	{
-		(*f)(i, s);
+		(*f)(i, s++);
 		i++;
 	}
 }
+
+/*
+	i = index
+	index starts at 0;
+	s starts at the index passed as a parameter (char *),
+	therefore the progression is different.
+*/
