4334a56d9693a71ab64b74225d4b19de773e392d
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-27 00:01:22 +0100

    updates

--- lvl_0_libft/ft_split.c
+++ lvl_0_libft/ft_split.c
@@ -6,29 +6,26 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 01:10:01 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/25 23:35:02 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/26 23:49:13 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static unsigned int	ft_nr_strs(char const *s, char c)
+static size_t	ft_nr_strs(char const *s, char c)
 {
 	size_t	i;
-	size_t	j;
 	size_t	nr_strs;
 
-	i = 0;
-	j = 0;
 	nr_strs = 0;
 	while (s[i])
 	{
-		while (s[j] != c && s[j])
-			j++;
+		while (s[i] == c)
+			i++;
 		nr_strs++;
-		j++;
-		i = j;
+		while (s[i] && s[i] != c)
+			i++;
 	}
 	return (nr_strs);
 }
@@ -45,26 +42,23 @@ char	**ft_split(char const *s, char c)
 	j = 0;
 	lines = 0;
 	nr_strs = ft_nr_strs(s, c);
-	matrix = malloc(nr_strs * sizeof(char *) + sizeof(NULL));
+	matrix = malloc((nr_strs + 1) * sizeof(char *));
 	if (!matrix)
 		return (NULL);
 	while (lines < nr_strs)
-	{
+	{	
+		while (s[j++] == c)
+			i++;
 		while (s[j] != c)
 			j++;
-		matrix[lines] = ft_substr(s, i, (j - i - 1) + 1);
+		matrix[lines] = ft_substr(s, i, ((j - 1) - i + 1));
+		i = j + 1;
 		j++;
-		i = j;
 		lines++;
 	}
 	matrix[lines] = NULL;
-	int	tumae;
-	for (tumae = 0; tumae <= 5; tumae++)
-		if (matrix[tumae] == NULL)
-			printf("(null)\n");
-		else
-			printf("%s\n", matrix[tumae]);
 	return (matrix);
 }
 
-/* j - i = s_len; s_len = tamanho da string que se pretende criar atualmente */
+/* (j - 1) - i) = s_len; 
+s_len = tamanho da string que se pretende criar atualmente */
