c6272628da8b2dbb6712b2538aa41389fded347b
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-27 22:25:38 +0100

    Updates

--- lvl_0_libft/ft_lstadd_front.c
+++ lvl_0_libft/ft_lstadd_front.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/22 20:22:43 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/27 22:19:57 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/27 19:30:53 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/27 22:25:27 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*new_node;
-
-	new_node = malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 }
