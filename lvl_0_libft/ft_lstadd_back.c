e0df33f2cd6157d69fdf5221010cf38a88a2ce17
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-28 04:33:31 +0100

    Updates

--- lvl_0_libft/ft_lstadd_back.c
+++ lvl_0_libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 22:52:35 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/27 23:57:41 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/08/28 04:33:16 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	if (!new)
 		return ;
-	if (!lst)
+	if (!*lst)
 	{
 		*lst = new;
 		return ;
