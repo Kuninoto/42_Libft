3430c9c84fdceb12bba09a8a0f77fe3ad500d9d4
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-08-29 00:51:06 +0100

    ft_split.c

--- lvl_0_libft/ft_split.c
+++ lvl_0_libft/ft_split.c
@@ -5,60 +5,76 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/20 01:10:01 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/26 23:49:13 by nnuno-ca         ###   ########.fr       */
+/*   Created: 2022/08/28 17:02:49 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/08/29 00:48:10 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static size_t	ft_nr_strs(char const *s, char c)
 {
-	size_t	i;
 	size_t	nr_strs;
+	size_t	new_str;
+	size_t	i;
 
 	nr_strs = 0;
+	i = 0;
 	while (s[i])
 	{
-		while (s[i] == c)
-			i++;
-		nr_strs++;
-		while (s[i] && s[i] != c)
-			i++;
+		if (s[i] != c && !new_str)
+		{
+			new_str = 1;
+			nr_strs++;
+		}
+		else if (s[i] == c)
+			new_str = 0;
+		i++;
 	}
 	return (nr_strs);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**matrix_writtin(char const *s, char c, char **matrix, size_t s_len)
 {
-	size_t			i;
-	size_t			j;
-	size_t			lines;
-	unsigned int	nr_strs;
-	char			**matrix;
+	size_t	i;
+	size_t	lines;
+	size_t	line_len;
 
 	i = 0;
-	j = 0;
 	lines = 0;
+	line_len = 0;
+	while (i < (s_len + 1))
+	{
+		if (s[i] == c || !s[i])
+		{
+			if (line_len > 0)
+			{
+				matrix[lines] = malloc((line_len + 1) * sizeof(char));
+				if (matrix[lines] != NULL)
+					ft_strlcpy(matrix[lines], &s[i - line_len], line_len + 1);
+				line_len = 0;
+				lines++;
+			}
+		}
+		else
+			line_len++;
+		i++;
+	}
+	return (matrix);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t			nr_strs;
+	char			**matrix;
+
+	if (!s)
+		return (NULL);
 	nr_strs = ft_nr_strs(s, c);
 	matrix = malloc((nr_strs + 1) * sizeof(char *));
 	if (!matrix)
 		return (NULL);
-	while (lines < nr_strs)
-	{	
-		while (s[j++] == c)
-			i++;
-		while (s[j] != c)
-			j++;
-		matrix[lines] = ft_substr(s, i, ((j - 1) - i + 1));
-		i = j + 1;
-		j++;
-		lines++;
-	}
-	matrix[lines] = NULL;
+	matrix_writtin(s, c, matrix, ft_strlen(s));
+	matrix[nr_strs] = NULL;
 	return (matrix);
 }
-
-/* (j - 1) - i) = s_len; 
-s_len = tamanho da string que se pretende criar atualmente */
