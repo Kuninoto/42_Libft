97b78b36df29cfdf2afeca1162b76d080d79b038
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-10-07 13:30:51 +0100

    Upgrades

--- lvl_0_libft/ft_isalpha.c
+++ lvl_0_libft/ft_isalpha.c
@@ -14,7 +14,9 @@
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z'))
 		return (1);
+	else if ((c >= 'a' && c <= 'z'))
+		return (2);
 	return (0);
 }
