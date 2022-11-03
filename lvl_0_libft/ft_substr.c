eca779e1aeb7cb56cdbbf1f06f80e15c0446823c
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-03 00:53:29 +0000

    ft_substr fix and split modification

--- lvl_0_libft/ft_substr.c
+++ lvl_0_libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 01:23:22 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/08/18 02:10:12 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/02 17:54:24 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*sub_str;
 	unsigned int	i;
 
-	if (*s == '\0')
+	if (!*s)
 		return (NULL);
+	if (start > ft_strlen(s))
+	{
+		sub_str = malloc(1 * sizeof(char));
+		if (!sub_str)
+			return (NULL);
+		sub_str[0] = '\0';
+		return (sub_str);
+	}
 	sub_str = malloc((len + 1) * sizeof(char));
-	if (sub_str == NULL)
+	if (!sub_str)
 		return (NULL);
 	i = 0;
 	while (i < len)
-	{
-		sub_str[i] = s[start];
-		start++;
-		i++;
-	}
+		sub_str[i++] = s[start++];
 	sub_str[i] = '\0';
 	return (sub_str);
 }
