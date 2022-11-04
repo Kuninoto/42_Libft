f6a938de0e4e88a22bec5d70d2ea94834132e893
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-04 18:56:01 +0000

    fixes

--- lvl_0_libft/ft_strnstr.c
+++ lvl_0_libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 00:12:36 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/04 01:12:13 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/04 15:52:08 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	if (!*big)
+	if ((!little || !big) && len == 0)
 		return (NULL);
 	if (!*little)
 		return ((char *)big);
+	i = 0;
 	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
