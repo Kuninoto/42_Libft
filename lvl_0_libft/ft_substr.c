99c7c188b113a5421fbf9640144f85f8f7f94b50
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-06 01:47:47 +0000

    final

--- lvl_0_libft/ft_substr.c
+++ lvl_0_libft/ft_substr.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
- 
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*sub_str;
