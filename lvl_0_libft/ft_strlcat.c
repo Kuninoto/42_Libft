0d9b19a928eaaca2bd778b673093b47cc5fbae03
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-06 01:02:57 +0000

    FINAL

--- lvl_0_libft/ft_strlcat.c
+++ lvl_0_libft/ft_strlcat.c
@@ -19,12 +19,12 @@ size_t	ft_strlcat(char *dest, char *src, size_t size)
 	size_t	j_dest;
 	size_t	i;
 
-	if (size == 0)
-		return (0);
 	len_dest = ft_strlen(dest);
 	len_src = ft_strlen(src);
 	j_dest = len_dest;
 	i = 0;
+	if (size == 0)
+		return (len_src);
 	if (size < len_dest)
 		return (len_src + size);
 	while (src[i] != '\0' && j_dest < (size - 1))
